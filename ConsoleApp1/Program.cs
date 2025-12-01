481bf8a1b77b881d51c5045278aa0fdc6df39714
CheikhMS <www.mouhamedcsa@gmail.com>
2025-12-01 00:28:06 +0000

    ajout ConsoleApp1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+using System;
+/// <summary>
+/// Programme pour afficher les informations d'un utlisateur
+/// </summary>
+class Program
+{
+    static void Main(string[] args)
+     {
+        Console.WriteLine("Prénom : ");
+        string prenom = Console.ReadLine();
+        Console.WriteLine("Nom : ");
+        string nom = Console.ReadLine();
+        Console.WriteLine("Email : ");
+        string email = Console.ReadLine();
+        Console.Write(" Bonjour {0} {1}, votre email est {2}", prenom, nom, email);
+    } 
+}
