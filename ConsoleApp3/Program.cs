7f07edf96e6b18aae5b4a5dc22cd3a1a1647fab6
Ngoné Diarisso <ngonediarissodk@groupeisi.com>
2025-12-06 22:03:24 +0000

    Ajout de l'application Appli3 (table de multiplication)

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,10 @@
+﻿// Partie 1 : Initialisation et saisie
+int nbre;
+bool rep = false;
+
+Console.WriteLine("Ce programme nous permet de faire la table de multiplication");
+
+do
+{
+    Console.WriteLine("Donner un nombre");
+    nbre = int.Parse(Console.ReadLine());
