b316273ec239600048c832ab9d207a7b32d79586
CheikhMS <www.mouhamedcsa@gmail.com>
2025-12-08 13:24:07 +0000

    Partie 3

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,15 +1,25 @@
 ﻿// Partie 1 : Initialisation et saisie
 int nbre;
 bool rep = false;
-
 Console.WriteLine("Ce programme nous permet de faire la table de multiplication");
 
 do
 {
-    Console.WriteLine("Donner un nombre");
+    Console.WriteLine("Donner un nombre : ");
     nbre = int.Parse(Console.ReadLine());
     // Partie 2 : Affichage de la table
     for (int i = 1; i <= 10; i++)
     {
         Console.WriteLine($"{nbre} * {i} = {nbre * i}");
     }
+    // Partie 3 : Recommencer ou pas
+    Console.WriteLine("Voulez-vous recommencer ? (O/N) : ");
+    if (Console.ReadLine().ToUpper() == "O")
+    {
+        rep = true;
+    }
+    else
+    {
+        rep = false;
+    }
+}while (rep == true);
